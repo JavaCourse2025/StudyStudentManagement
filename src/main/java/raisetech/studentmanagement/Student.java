19f1317057af1e5476b9bd0a3f99d9c1d54dc730
JavaCourse2025 <myk.sgmt726@gmail.com>
2025-10-23 15:55:45 +0900

    StudentCoursesクラス作成

---
+++ src/main/java/raisetech/studentmanagement/Student.java
@@ -0,0 +1,20 @@
+package raisetech.studentmanagement;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Student {
+
+    private String id;
+    private String fullName;
+    private String furigana;
+    private String nickName;
+    private String email;
+    private String region;
+    private String gender;
+    private int age;
+}
+
---
+++ src/main/java/raisetech/studentmanagement/StudentRepository.java
@@ -0,0 +1,15 @@
+package raisetech.studentmanagement;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface StudentRepository {
+
+    @Select("SELECT * FROM students")
+    List<Student> search();
+
+
+}
