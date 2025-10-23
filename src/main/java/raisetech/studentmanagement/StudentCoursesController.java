5d7c29929bab234a546baeb35ace5fc62a54a850
JavaCourse2025 <myk.sgmt726@gmail.com>
2025-10-23 16:15:24 +0900

    StudentCoursesControllerクラス作成

---
+++ src/main/java/raisetech/studentmanagement/StudentCoursesController.java
@@ -0,0 +1,19 @@
+package raisetech.studentmanagement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentCoursesController {
+    @Autowired
+    private StudentCoursesRepository repository;
+
+    @GetMapping("/studentsCoursesList")
+    public List<StudentsCourses> getStudentCourses() {
+        return repository.search();
+
+    }
+}
