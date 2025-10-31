1fe494aab940b197f514ad89bd8b5a4b13908c72
JavaCourse2025 <myk.sgmt726@gmail.com>
2025-10-31 12:00:40 +0900

    StudentCoursesControllerがServiceを経由するよう修正

--- src/main/java/raisetech/studentmanagement/controller/StudentCoursesController.java
+++ src/main/java/raisetech/studentmanagement/controller/StudentCoursesController.java
@@ -1,21 +1,24 @@
 package raisetech.studentmanagement.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.studentmanagement.data.StudentsCourses;
-import raisetech.studentmanagement.repository.StudentCoursesRepository;
+import raisetech.studentmanagement.service.StudentService;
 
 import java.util.List;
 
 @RestController
 public class StudentCoursesController {
-    @Autowired
-    private StudentCoursesRepository repository;
+
+    private StudentService service;
+
+    public StudentCoursesController(StudentService service) {
+        this.service = service;
+    }
 
     @GetMapping("/studentsCoursesList")
     public List<StudentsCourses> getStudentCourses() {
-        return repository.search();
+        return service.searchStudentCourses();
 
     }
 }
