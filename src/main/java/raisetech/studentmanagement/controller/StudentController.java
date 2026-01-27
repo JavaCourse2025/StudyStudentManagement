b334451505b5a03d49ae792cb222cc321537e29c
JavaCourse2025 <myk.sgmt726@gmail.com>
2026-01-27 10:18:51 +0900

    List<StudentDetail>内 StudentController.javaからStudentConverter.javaに移動

--- src/main/java/raisetech/studentmanagement/controller/StudentController.java
+++ src/main/java/raisetech/studentmanagement/controller/StudentController.java
@@ -3,22 +3,23 @@ package raisetech.studentmanagement.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.studentmanagement.controller.converter.StudentConverter;
 import raisetech.studentmanagement.data.Student;
 import raisetech.studentmanagement.data.StudentsCourses;
 import raisetech.studentmanagement.domain.StudentDetail;
 import raisetech.studentmanagement.service.StudentService;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class StudentController {
     private StudentService service;
+    private StudentConverter converter;
 
     @Autowired
-    public StudentController(StudentService service) {
+    public StudentController(StudentService service, StudentConverter converter) {
         this.service = service;
+        this.converter = converter;
     }
 
     @GetMapping("/studentList")
@@ -27,25 +28,8 @@ public class StudentController {
         List<Student> students = service.searchStudentList();
         List<StudentsCourses> studentsCourses = service.searchStudentCoursesList();
 
-        return convertStudentDetails(students, studentsCourses);
-
+        return converter.convertStudentDetails(students, studentsCourses);
 
     }
-
-    private List<StudentDetail> convertStudentDetails(List<Student> students, List<StudentsCourses> studentsCourses) {
-        List<StudentDetail> studentDetails = new ArrayList<>();
-        students.forEach(student -> {
-            StudentDetail studentDetail = new StudentDetail();
-            studentDetail.setStudent(student);
-
-            List<StudentsCourses> convertStudentCourses = studentsCourses.stream()
-                    .filter(studentsCourse -> student.getId().equals(String.valueOf(studentsCourse.getStudentId())))
-                    .collect(Collectors.toList());
-            studentDetail.setStudentsCourses(convertStudentCourses);
-            studentDetails.add(studentDetail);
-        });
-        return studentDetails;
-    }
-
 }
 
